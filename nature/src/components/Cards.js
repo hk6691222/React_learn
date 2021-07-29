c787e41195a8e7c6ea3a022ff57962b4f7f2d356
Himanshu <hk6691222@gmail.com>
2021-07-29 12:22:41 +0530

    y commit

--- nature/src/components/Cards.js
+++ nature/src/components/Cards.js
@@ -9,37 +9,23 @@ function Cards() {
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
-            <CardItem 
-              src='src/components/PICS/Autum trees.jpg'
+            <CardItem
+              src="src/components/PICS/Autum trees.jpg"
               text="Explore the top visiting place in Indian Jungles"
               label="Jungles"
               path="/sign-up"
             />
             <CardItem
-              src='src/components/PICS/sunflower.jpg'
+              src="src/components/PICS/sunflower.jpg"
               text="Beautiful Sunflower park with colorful flowers"
               label="Flowers"
               path="/products"
             />
-          </ul>
-          <ul className="cards__items">
             <CardItem
-              src='src/components/PICS/amazing-animal.jpg'
+              src="src/components/PICS/amazing-animal.jpg"
               text="Top visiting place in big Zoo"
               label="Jungles"
               path="/services"
-            />
-            <CardItem
-              src='src/components/PICS/bora bora.jpg'
-              text="Exploring beautiful Maldives enjoying beaches"
-              label="Beach"
-              path="/products"
-            />
-            <CardItem
-              src='src/components/PICS/Capri Travel Guide.png'
-              text="Amazing travelling areas in beautiful south africa places"
-              label="Walking"
-              path="/sign-up"
             />
           </ul>
         </div>
